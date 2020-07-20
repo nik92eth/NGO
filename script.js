70e16533b42c3622792f43f2234ca056beba8711
Nik92 <nikhil.arora564@gmail.com>
2020-07-21 04:26:48 +0530

    ver 1.0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
+  if (!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
   } else {
@@ -14,17 +14,62 @@ menuBtn.addEventListener('click', () => {
 
 // NAVBAR CLOSES AFTER CLICKING ON LINKS IN MOBILE VIEW
 var elements = document.getElementsByClassName("nav-link");
- 
- var myFunction = function() {
-   if(!menuOpen) {
-      menuBtn.classList.add('open');
-      menuOpen = true;
-    } else {
-      menuBtn.classList.remove('open');
-      menuOpen = false;
+
+var myFunction = function () {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+};
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', myFunction, false);
+}
+
+
+
+const navBg = document.querySelectorAll(".fixed-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    if (!navBg[0].classList.contains("scrolled")) {
+      navBg[0].classList.add("scrolled");
+      navBg[1].classList.add("scrolled");
     }
- };
- 
- for (var i = 0; i < elements.length; i++) {
-     elements[i].addEventListener('click', myFunction, false);
- }
+  } else {
+    if (navBg[0].classList.contains("scrolled")) {
+      navBg[0].classList.remove("scrolled");
+      navBg[1].classList.remove("scrolled");
+    }
+  }
+  scrollFunction();
+});
+
+
+
+// SMOOTH SCROLLING SECTIONS
+$('a[href*=#]:not([href=#])').click(function () {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    || location.hostname == this.hostname) {
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
+
+
+// if('#vision'.onClick()){
+//   $("#mission").addClass("d-none");
+//   $("#vision").removeClass("d-none");
+//   $("#objectives").addClass("d-none");
+// }
